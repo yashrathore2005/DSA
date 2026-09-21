db56fc8fd4056665e68794805710c9e37c47bc8e
yashrathore2005 <132598248+yashrathore2005@users.noreply.github.com>
2026-09-21 16:43:51 +0530

    Create Remove_all_Adjacent_Duplicates_in_String.java

---
+++ Day-5/Remove_all_Adjacent_Duplicates_in_String.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == ch) {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (char ch : stack) {
+            result.append(ch);
+        }
+
+        return result.toString();
+    }
+}
